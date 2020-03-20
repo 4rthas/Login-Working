9e80335256becc6297f01a6f22de08e790123321
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-20 11:36:19 +0100

    add selector + event + listener on blur for fields of form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 var app = {
-    init: function() {
-      console.log('app.init'); 
+
+  // select all elements with .field-input class and store them in fields (array)
+  fields: document.querySelectorAll('.field-input'),
+
+  init: function() {
+    console.log('app.init');
+
+    // loop on fields array
+    for(let i = 0; i < app.fields.length; i++){
+
+      console.log(app.fields[i]);
+
+      // store current field
+      let currentField = app.fields[i];
+
+      // add listener on blur (lose focus) for currentFieldement sur blur and add function (isInputValid)
+      currentField.addEventListener('blur', app.isInputValid);
     }
-  };
-  
-  document.addEventListener('DOMContentLoaded', app.init);
+  },
+
+  // waveEvent or evt or e
+  isInputValid: function (waveEvent){
+
+    console.log(waveEvent);
+  },
+};
+
+
+
+document.addEventListener('DOMContentLoaded', app.init);
